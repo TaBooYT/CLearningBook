cb419e68f4759a93f7ce4844c930a10e6704280c
vladimir_zheltouhov <vova-zheltoukhov@mail.ru>
2025-10-27 18:48:50 +0700

    Tic-tac-toe rabota s ciklamy i strokamy

--- CLearningBook/CLearningBook.cpp
+++ CLearningBook/CLearningBook.cpp
@@ -4,39 +4,31 @@
 using namespace std;
 int main()
 {
-	const int MAX_ITEMS = 10;
-	string inventory[MAX_ITEMS];
-	int numItems = 0;
-	inventory[numItems++] = "sword";
-	inventory[numItems++] = "armor";
-	inventory[numItems++] = "shield";
-	cout << "Your items: \n";
-	for (int i = 0; i < numItems; i++)
+	const int ROWS = 3;
+	const int COLUMNS = 3;
+	char board[ROWS][COLUMNS] = { {'O', 'X', 'O'},
+								{' ', 'X', 'X'},
+								{'X', 'O', 'O'}, };
+	cout << "Here's the tic-tac-toe board:\n";
+	for (int i = 0; i < ROWS; i++)
 	{
-		cout << inventory[i] << endl;
+		for (int j = 0; j < COLUMNS; j++)
+		{
+			cout << board[i][j];
+		}
+		cout << endl;
 	}
-	cout << "\nYou trade your sword for a battle axe.";
-	inventory[0] = "battle axe";
-	cout << "\nyour items:\n";
-	for (auto i = 0; i < numItems; i++)
+	cout << "\n'X' moves to the empty location.\n\n";
+	board[1][0] = 'X';
+	cout << "Now the tic-tac-toe board is:\n";
+	for (int i = 0; i < ROWS; i++)
 	{
-		cout << inventory[i] << endl;
+		for (int j = 0; j < COLUMNS; j++)
+		{
+			cout << board[i][j];
+		}
+		cout << endl;
 	}
-	cout << "\nThe item name '" << inventory[0] << "' has ";
-	cout << inventory[0].size() << " letters in it.\n";
-	cout << "\nypu find a healding potion.";
-	if (numItems < MAX_ITEMS)
-	{
-		inventory[numItems++] = "healing potion";
-	}
-	else
-	{
-		cout << "You have too many items and can't carry another.";
-	}
-	cout << "\nYour items:\n";
-	for (int i = 0; i < numItems; i++)
-	{
-		cout << inventory[i] << endl;
-	}
+	cout << "\n'X' wins!";
 	return 0;
 }
