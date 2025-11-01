330e97052f99323ab1e5f3debb57bc14babe67c2
vladimir_zheltouhov <vova-zheltoukhov@mail.ru>
2025-11-01 20:08:07 +0700

    Game hangman

--- CLearningBook/CLearningBook.cpp
+++ CLearningBook/CLearningBook.cpp
@@ -4,53 +4,76 @@
 #include <algorithm>
 #include <ctime>
 #include <cstdlib>
+#include <cctype>
 
 using namespace std;
 int main()
 {
-	vector<int>::const_iterator iter;
-	cout << "Creating a list of scores.";
-	vector<int> scores;
-	scores.push_back(1500);
-	scores.push_back(3500);
-	scores.push_back(7500);
-	cout << "\nHight Scores;\n";
-	for (iter = scores.begin(); iter != scores.end(); ++iter)
-	{
-		cout << *iter << endl;
-	}
-	cout << "\nFinding a score.";
-	int score;
-	cout << "\nEnter a score to find: ";
-	cin >> score;
-	iter = find(scores.begin(), scores.end(), score);
-	if (iter != scores.end())
-	{
-		cout << "Score found.\n";
-	}
-	else
-	{
-		cout << "Score not found.\n";
-	}
-	cout << "\nRandomizing scores.";
+	const int MAX_WRONG = 8;
+	vector<string>words;
+	words.push_back("GUESS");
+	words.push_back("HANGMAN");
+	words.push_back("DIFFICULT");
 	srand(static_cast<unsigned int>(time(0)));
-	random_shuffle(scores.begin(), scores.end());
-	cout << "\nHight Scores:\n";
-	for (iter = scores.begin(); iter != scores.end(); ++iter)
+	random_shuffle(words.begin(), words.end());
+	const string THE_WORD = words[0];
+	int wrong = 0;
+	string soFar(THE_WORD.size(), '-');
+	string used = "";
+	cout << "Welcome to Hangman. Good luck!\n";
+	while ((wrong < MAX_WRONG) && (soFar != THE_WORD))
 	{
-		cout << *iter << endl;
+		cout << "\n\nYou have " << (MAX_WRONG - wrong);
+		cout << " incorrect guess left.\n";
+		cout << "\nYou've used the following letters:\n" << used << endl;
+		cout << "\nFo far. the word is:\n" << soFar << endl;
+		char guess;
+		cout << "\n\nEnter your guess: ";
+		cin >> guess;
+		guess = toupper(guess);
+
+		while (used.find(guess))
+		{
+			cout << "\nYou've already guessed " << guess << endl;
+			cout << "Enter your guess: ";
+			cin >> guess;
+			guess = toupper(guess);
+		}
+		used += guess;
+		if (THE_WORD.find(guess) != string::npos)
+		{
+			cout << "That's right! " << guess << " is in the word.\n";
+			for (int i = 0; i < THE_WORD.length(); i++)
+			{
+				if (THE_WORD[i] == guess)
+				{
+					soFar[i] = guess;
+				}
+			}
+		}
+		else
+		{
+			cout << "Sorry. " << guess << " isn't in the word.\n";
+			++wrong;
+		}
+
+		if (wrong == MAX_WRONG)
+		{
+			cout << "\nYou've been hanged!";
+		}
+		else
+		{
+			cout << "\nYou guessed it!";
+		}
+		cout << "\nThe word was " << THE_WORD << endl;
+		return 0;
 	}
-	cout << "\nSorting scores.";
-	sort(scores.begin(), scores.end());
-	cout << "\nHight Scores:\n";
-	for (iter = scores.begin(); iter != scores.end(); ++iter)
-	{
-		cout << *iter << endl;
-	}
 
 
 
-	return 0;
+
+
+	
 }
 
 
