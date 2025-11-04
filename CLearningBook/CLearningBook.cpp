f7bcb425d11a23935b7293966f7eca935078a7a9
vladimir_zheltouhov <vova-zheltoukhov@mail.ru>
2025-11-04 18:29:24 +0700

    Programm Instruction

--- CLearningBook/CLearningBook.cpp
+++ CLearningBook/CLearningBook.cpp
@@ -1,81 +1,20 @@
 ﻿#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <ctime>
-#include <cstdlib>
-#include <cctype>
 
 using namespace std;
+
+void Instruction();
+
 int main()
 {
-	const int MAX_WRONG = 8;
-	vector<string>words;
-	words.push_back("GUESS");
-	words.push_back("HANGMAN");
-	words.push_back("DIFFICULT");
-	srand(static_cast<unsigned int>(time(0)));
-	random_shuffle(words.begin(), words.end());
-	const string THE_WORD = words[0];
-	int wrong = 0;
-	string soFar(THE_WORD.size(), '-');
-	string used = "";
-	cout << "Welcome to Hangman. Good luck!\n";
-	while ((wrong < MAX_WRONG) && (soFar != THE_WORD))
-	{
-		cout << "\n\nYou have " << (MAX_WRONG - wrong);
-		cout << " incorrect guess left.\n";
-		cout << "\nYou've used the following letters:\n" << used << endl;
-		cout << "\nFo far. the word is:\n" << soFar << endl;
-		char guess;
-		cout << "\n\nEnter your guess: ";
-		cin >> guess;
-		guess = toupper(guess);
-
-		while (used.find(guess))
-		{
-			cout << "\nYou've already guessed " << guess << endl;
-			cout << "Enter your guess: ";
-			cin >> guess;
-			guess = toupper(guess);
-		}
-		used += guess;
-		if (THE_WORD.find(guess) != string::npos)
-		{
-			cout << "That's right! " << guess << " is in the word.\n";
-			for (int i = 0; i < THE_WORD.length(); i++)
-			{
-				if (THE_WORD[i] == guess)
-				{
-					soFar[i] = guess;
-				}
-			}
-		}
-		else
-		{
-			cout << "Sorry. " << guess << " isn't in the word.\n";
-			++wrong;
-		}
-
-		if (wrong == MAX_WRONG)
-		{
-			cout << "\nYou've been hanged!";
-		}
-		else
-		{
-			cout << "\nYou guessed it!";
-		}
-		cout << "\nThe word was " << THE_WORD << endl;
-		return 0;
-	}
-
-
-
-
-
-	
+	Instruction();
+	return 0;
 }
 
+void Instruction()
+{
+	cout << "Welcome to the most fun you've ever had with text!\n\n";
+	cout << "Here's how to play the game...\n";
+}
 
 
 /*
