dbe44d00619ee3cfabc96b8c3f66c4d9f4a28e05
vladimir_zheltouhov <vova-zheltoukhov@mail.ru>
2025-12-21 22:01:32 +0700

    Global Reach

--- CLearningBook/CLearningBook.cpp
+++ CLearningBook/CLearningBook.cpp
@@ -1,21 +1,35 @@
 ﻿#include <iostream>
 
 using namespace std;
-
-void Instruction();
+int glob = 10;
+void access_global();
+void hide_global();
+void change_global();
 
 int main()
 {
-	Instruction();
+	cout << "In main() glob is: " << glob << "\n\n";
+	access_global();
+	hide_global();
+	cout << "In main() glob is: " << glob << "\n\n";
+	change_global();
+	cout << "In main() glob is: " << glob << "\n\n";
 	return 0;
 }
-
-void Instruction()
+void access_global()
 {
-	cout << "Welcome to the most fun you've ever had with text!\n\n";
-	cout << "Here's how to play the game...\n";
+	cout << "In access_global() glob is: " << glob << "\n\n";
 }
-
+void hide_global()
+{
+	int glob = 0; // скрываем глобальную переменную glob
+	cout << "In hide_global() glob is: " << glob << "\n\n";
+}
+void change_global()
+{
+	glob = 10; // изменяем глобальную переменную glob
+	cout << "In change_global() glob is: " << glob << "\n\n";
+}
 
 /*
 d:
