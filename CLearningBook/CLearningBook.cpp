a4c8f48a115b7d3c1bc7b5d75dd71483b45d757d
vladimir_zheltouhov <vova-zheltoukhov@mail.ru>
2025-12-27 18:21:47 +0700

    Give Me a Number

--- CLearningBook/CLearningBook.cpp
+++ CLearningBook/CLearningBook.cpp
@@ -1,34 +1,28 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
-int glob = 10;
-void access_global();
-void hide_global();
-void change_global();
+
+int askNumber(int high, int low = 1);
 
 int main()
 {
-	cout << "In main() glob is: " << glob << "\n\n";
-	access_global();
-	hide_global();
-	cout << "In main() glob is: " << glob << "\n\n";
-	change_global();
-	cout << "In main() glob is: " << glob << "\n\n";
+	int number = askNumber(5);
+	cout << "Thanks for entering: " << number << "\n\n";
+	number = askNumber(10, 5);
+	cout << "Thanks for entering: " << number << "\n\n";
 	return 0;
 }
-void access_global()
+
+int askNumber(int high, int low)
 {
-	cout << "In access_global() glob is: " << glob << "\n\n";
-}
-void hide_global()
-{
-	int glob = 0; // скрываем глобальную переменную glob
-	cout << "In hide_global() glob is: " << glob << "\n\n";
-}
-void change_global()
-{
-	glob = 10; // изменяем глобальную переменную glob
-	cout << "In change_global() glob is: " << glob << "\n\n";
+	int num;
+	do
+	{
+		cout << "Please enter a number" << " (" << low << " - " << high << "): ";
+		cin >> num;
+	} while (num > high || num < low);
+	return num;
 }
 
 /*
