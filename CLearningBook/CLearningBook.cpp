39cde172a80e610c05f62e7c7d4a78689259da9b
vladimir_zheltouhov <vova-zheltoukhov@mail.ru>
2026-01-15 23:31:10 +0700

    Taking Damage

--- CLearningBook/CLearningBook.cpp
+++ CLearningBook/CLearningBook.cpp
@@ -1,24 +1,22 @@
 ﻿#include <iostream>
-#include <string>
-
+int radiation(int health);
 using namespace std;
-
-int triple(int number);
-string triple(string text);
 
 int main()
 {
-	cout << "Tripling 5: " << triple(5) << "\n\n";
-	cout << "Tripling 'gamer': " << triple("gamer");
+	int health = 80;
+	cout << "Your health is " << health << "\n\n";
+	health = radiation(health);
+	cout << "After radiation exposure your health is " << health << "\n\n";
+	health = radiation(health);
+	cout << "After radiation exposure your health is " << health << "\n\n";
+	health = radiation(health);
+	cout << "After radiation exposure your health is " << health << "\n\n";
 	return 0;
 }
-int triple(int number)
+inline int radiation(int health)
 {
-	return (number * 3);
-}
-string triple(string text)
-{
-	return (text + text + text);
+	return (health / 2);
 }
 
 /*
