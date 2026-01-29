36043040f8250dd6224dad826b0d3b494a054785
TaBooYT <tabootaboo200@gmail.com>
2026-01-29 18:38:13 +0700

    tic-tac-toe -- no work -- error in main in while

--- CLearningBook/CLearningBook.cpp
+++ CLearningBook/CLearningBook.cpp
@@ -1,40 +1,257 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
-// возвращает ссылку на строку
-string& refToElement(vector<string>& inventory, int i);
-
+// глобальные константы
+const char X = 'X';
+const char O = 'O';
+const char EMPTY = ' ';
+const char TIE = 'T';
+const char NO_ONE = 'N';
+// прототипы функций
+void instructions();
+char askYesNo(string question);
+int askNumber(string question, int high, int low = 0);
+char humanPiece();
+char opponent(char piece);
+void displayBoard(const vector<char>& board);
+char winner(const vector<char>& board);
+inline bool isLegal(const vector<char>& board, int move);
+int humanMove(const vector<char>& board, char human);
+int computerMove(vector<char> board, char computer);
+void announceWinner(char winner, char computer, char human);
+// функция main
 int main()
 {
-	vector<string> inventory;
-	inventory.push_back("sword");
-	inventory.push_back("armor");
-	inventory.push_back("shield");
-	// отображается строка. на которую указывает возвращенная ссылка
-	cout << "Sending the returned reference to cout:\n";
-	cout << refToElement(inventory, 0) << "\n\n";
-	// присвает одну ссылку другой - малозатратная операция присвания
-	cout << "Assigning the returned reference to another reference.\n";
-	string& rStr = refToElement(inventory, 1);
-	cout << "Sending the new reference to cout:\n";
-	cout << rStr << "\n\n";
-	// копирует строковый объект - затратная операция присваивания
-	cout << "Assigning the returned reference to a string odject.\n";
-	string str = refToElement(inventory, 2);
-	cout << "Sending the new string object to cout:\n";
-	cout << str << "\n\n";
-	// изменение строкового объекта посредством возвращенной ссылки
-	cout << "Altering an object through a returned reference.\n";
-	rStr = "Healing Potion";
-	cout << "Sending the altered object to cout:\n";
-	cout << inventory[1] << endl;
+	int move;
+	const int NUM_SQUARES = 9;
+	vector<char> board(NUM_SQUARES, EMPTY);
+	instructions();
+	char human = humanPiece();
+	char computer = opponent(human);
+	char turn = X;
+	displayBoard(board);
+	while (winner(board) == NO_ONE)
+	{
+		if (turn == human)
+		{
+			move = humanMove(board, human);
+			board[move] = human;
+		}
+		else
+		{
+			move = computerMove(board, computer);
+			board[move] = computer;
+		}
+		displayBoard(board);
+		turn = opponent(turn);
+	}
+	announceWinner(winner(board), computer, human);
 	return 0;
 }
-// возвращает ссылку на строку
-string& refToElement(vector<string>& vec, int i)
+
+void instructions()
 {
-	return vec[i];
+	cout << "Welcome to the ultimate man-machine showdown: Tic-Tac-Toe.\n";
+	cout << "..where human brain is pit against silicon processor\n\n";
+	cout << "Make your move know by entering a number. 0 - 8. The number\n";
+	cout << "corresponds to the desired board position. as illustrated:\n\n";
+	cout << " 0 | 1 | 2\n";
+	cout << "----------\n";
+	cout << " 3 | 4 | 5\n";
+	cout << "----------\n";
+	cout << " 6 | 7 | 8\n";
+	cout << "Prepare yourself. human. The battle is about to begin.\n\n";
+}
+
+char askYesNo(string question)
+{
+	char response;
+	do
+	{
+		cout << question << " (y/n): ";
+		cin >> response;
+	} while (response != 'y' && response != 'n');
+	return response;
+}
+
+int askNumber(string question, int high, int low)
+{
+	int number;
+	do
+	{
+		cout << question << " (" << low << " - " << high << "): ";
+		cin >> number;
+	} while (number > high || number < low);
+	return number;
+}
+
+char humanPiece()
+{
+	char go_first = askYesNo("Do you require the fist move?");
+	if (go_first == 'y')
+	{
+		cout << "\nThen take the first move. You will need it.\n";
+		return X;
+	}
+	else
+	{
+		cout << "\nYour bravery will be your undoing...I will go first.\n";
+		return 0;
+	}
+}
+
+char opponent(char piece)
+{
+	if (piece == X)
+	{
+		return 0;
+	}
+	else
+	{
+		return X;
+	}
+}
+
+void displayBoard(const vector<char>& board)
+{
+	cout << "\n\t" << board[0] << " | " << board[1] << " | " << board[2];
+	cout << "\n\t" << "-------------";
+	cout << "\n\t" << board[3] << " | " << board[4] << " | " << board[5];
+	cout << "\n\t" << "-------------";
+	cout << "\n\t" << board[6] << " | " << board[7] << " | " << board[8];
+	cout << "\n\n";
+}
+
+char winner(const vector<char>& board)
+{
+	// все возможные выигрышные ряды
+	const int WINNING_ROWS[8][3] = {
+		{0, 1, 2},
+		{6, 7, 8},
+		{0, 3, 6},
+		{1, 4, 7},
+		{2, 5, 8},
+		{0, 4, 8},
+		{3, 4, 5},
+		{2, 4, 6}
+	};
+	const int TOTAL_ROWS = 8;
+	// если в одном из выигрышных рядов уже присутствуют три одинаковых значения
+	// (причем они не равны EMPTY), то победитель определился
+	for (int row = 0; row < TOTAL_ROWS; row++)
+	{
+		if ((board[WINNING_ROWS[row][0]] != EMPTY) &&
+			(board[WINNING_ROWS[row][0]] == board[WINNING_ROWS[row][1]]) &&
+			(board[WINNING_ROWS[row][1]] == board[WINNING_ROWS[row][2]]))
+		{
+			return board[WINNING_ROWS[row][0]];
+		}
+	}
+
+	// страница 205 остановка
+
+	// Поскольку победитель не определился, проверяем, не наступила ли ничья
+	// (остались ли на поле пустые клетки)
+	if (count(board.begin(), board.end(), EMPTY) == 0)
+	{
+		return NO_ONE;
+	}
+}
+
+inline bool isLegal(int move, const vector<char>& board)
+{
+	return (board[move] == EMPTY);
+}
+
+int humanMove(const vector<char>& board, char human)
+{
+	int move = askNumber("Where will you move?", (board.size() - 1));
+	while (!isLegal(move, board))
+	{
+		cout << "\nThat square is already occupied. floolish human.\n";
+		move = askNumber("Where will you move?", (board.size() - 1));
+	}
+	cout << "Fine...\n";
+	return move;
+}
+
+int computerMove(vector<char> board, char computer)
+{
+	unsigned int move = 0;
+	bool found = false;
+	//если компьютер может выиграть следующим ходом, то он делает этот ход
+	while (!found && move < board.size())
+	{
+		if (isLegal(move, board))
+		{
+			board[move] = computer;
+			found = winner(board) == computer;
+			board[move] = EMPTY;
+		}
+		if (!found)
+		{
+			++move;
+		}
+	}
+
+	// иначе, если человек может победить следующим ходом, блокировать этот ход
+	if (!found)
+	{
+		move = 0;
+		char human = opponent(computer);
+		while (!found && move < board.size())
+		{
+			if (isLegal(move, board))
+			{
+				board[move] = human;
+				found = winner(board) == human;
+				board[move] = EMPTY;
+			}
+			if (!found)
+			{
+				++move;
+			}
+		}
+	}
+
+	// иначе занять следующим ходом оптимальную свободную клетку
+	if (!found)
+	{
+		move = 0;
+		unsigned int i = 0;
+		const int BEST_MOVES[] = { 4, 0, 2, 6, 8, 1, 3, 5, 7 };
+		// выбрать оптимальную свободную клетку
+		while (!found && i < board.size())
+		{
+			move = BEST_MOVES[i];
+			if (isLegal(move, board))
+			{
+				found = true;
+			}
+			++i;
+		}
+	}
+
+	cout << "I shall take square number " << move << endl;
+	return move;
+}
+
+void announceWinner(char winner, char computer, char human)
+{
+	if (winner == computer)
+	{
+		cout << winner << "'s won!\n";
+		cout << "As I predicted, human. I am triumphant once more -- proof\n";
+		cout << "that computers are superior to humans in all regards.\n";
+	}
+	else if (winner == human)
+	{
+		cout << winner << "'s won!\n";
+		cout << "No, no! It cannot be! Somehow you tricked me, human.\n";
+		cout << "But never again! I, the computer, so swear it!\n";
+	}
 }
 
 /*
