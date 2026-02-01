38ae9f6b7617d78251fbaa76c83be4855abe107b
TaBooYT <tabootaboo200@gmail.com>
2026-02-01 14:54:34 +0700

    private critter

--- CLearningBook/CLearningBook.cpp
+++ CLearningBook/CLearningBook.cpp
@@ -4,25 +4,44 @@ using namespace std;
 
 class Critter
 {
-public:
-	int m_Hunger;
+public: // начало открытого раздела
 	Critter(int hunger = 0);
-	void Greet();
+	int GetHunger() const;
+	void SetHunger(int hunger);
+private: // начало закрытого раздела 
+	int m_Hunger;
 };
-Critter::Critter(int hunger)
+Critter::Critter(int hunger):
+m_Hunger(hunger)
 {
 	cout << "A new critter has been born!" << endl;
-	m_Hunger = hunger;
 }
-void Critter::Greet()
+int Critter::GetHunger() const
 {
-	cout << "Hi, I'm a critter. My hunger level is " << m_Hunger << ".\n";
+	return m_Hunger;
+}
+void Critter::SetHunger(int hunger)
+{
+	if (hunger < 0)
+	{
+		cout << "You can't set a critter's hunger to a negative number.\n\n";
+	}
+	else
+	{
+		m_Hunger = hunger;
+	}
 }
 
 int main()
 {
-	Critter crit(7);
-	crit.Greet();
+	Critter crit(5);
+	//cout << crit.m_Hunger; // недопустимо, член m_Hunger является закрытым!
+	cout << "Calling GetHunger();" << crit.GetHunger() << "\n\n";
+	cout << "Calling SetHunger(); with -1.\n";
+	crit.SetHunger(-1);
+	cout << "Calling SetHunger() with 9.\n";
+	crit.SetHunger(9);
+	cout << "Calling GetHunger(): " << crit.GetHunger() << "\n\n";
 	return 0;
 }
 
